1b8894a8cb312c7e6b27f4827862dc93dc6681db
aazadi <aazadi@gmail.com>
2020-03-06 12:18:01 +0100

    index

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,24 +7,45 @@ function res_img(...args){
     var img = args[1];
     var alt= args[2];
     var sizes = args[3];
+    var classes = args[4] || ' ';
+    console.log(args);
+    console.log(classes);
     var ress = "1920 1600 1366 1024 768 640 400 320 160".split(' ');
     var ret="<img src='"+ hexo.config.root + path + img + "' \n";
-
+    ret += "\n class='" + classes + "'";
     ret += "\n alt='" + alt + "'";
     ret += "\n srcset='" + ress.map( r => " " + hexo.config.root + path + r + "_" + img + " " + r + "w").join(",\n") + "' ";
     ret += "\n sizes='" + sizes + "'>";
     return ret;
 }
+
+hexo.extend.helper.register('cover_img', function(slug, classes= ''){
+    var relativeDir = path.join('images', 'posts', slug);
+    var thumb = path.join(hexo.source_dir,  relativeDir, 'thumb.jpg');
+    console.log(classes);
+    if (!fs.ensurePath(thumb)) return "";
+    try {
+
+        return res_img(relativeDir+'\\', 'thumb.jpg' , "Cover for "+slug,  "(max-width: 450px) 100vw, (max-width: 1200px) 50vw, 600px", classes);
+        // var photos = files.map(f => relativeDir + '/' + 'small_'+f)
+    } catch (e) {
+        if (hexo.env.debug) console.log(e);
+        return ''
+    }
+
+});
+
+
 hexo.extend.helper.register('res_img', function(...args) {
     return res_img(args);
 });
 
-hexo.extend.helper.register('gallery', function(args) {
-    var relativeDir = path.join('images', 'galleries', args);
+hexo.extend.helper.register('gallery', function(slug) {
+    var relativeDir = path.join('images', 'posts', slug, 'gallery');
 
     var dir = path.join(hexo.source_dir,  relativeDir);
     if (!fs.exists(dir)) return "";
-    var themePath = path.join(hexo.theme_dir, 'layout', '_partial', 'top-gal.ejs')
+    var themePath = path.join(hexo.theme_dir, 'layout', '_partial', 'top-gal.ejs');
 
     if (hexo.env.debug) {
         console.log('[Autogallery] Running autogallery on ' +  dir + ', using layout ' + themePath);
@@ -34,7 +55,7 @@ hexo.extend.helper.register('gallery', function(args) {
         var files = fs.listDirSync(dir);
         var template = fs.readFileSync(themePath);
 
-        var photos = files.map(f => res_img(relativeDir+'\\', f , "A film shot",  "(max-width: 450px) 100vw, (max-width: 1200px) 30vw, 400px"));
+        var photos = files.map(f => res_img(relativeDir+'\\', f , "A film shot",  "(max-width: 450px) 100vw, (max-width: 1200px) 50vw, 600px"));
         // var photos = files.map(f => relativeDir + '/' + 'small_'+f)
 
         return ejs.render(template, {
--- source/_posts/backpacking-tales.md
+++ source/_posts/backpacking-tales.md
@@ -1,7 +1,13 @@
 ---
-title: backpacking-tales
+title: Backpacking Tales
 date: 2020-02-21 22:44:40
 tags:
 ---
-ogff
-jajaja
+Germany | 2017 | 3′ | crowdfunding video | 16:9
+Crowdfunding video for [AphorismA Verlag](https://www.aphorisma.eu/)
+German Titel: Märchen im Gepäck
+7,505 € raised.
+
+Campaign hosted on: [Startnext](https://www.startnext.com/en/maerchen-im-gepaeck)
+
+‘Backpacking Tales’ is a collection of inspiring and unknown Arabic fairy tales in a German-Arabic illustrated publication with the intention to convey the imagery of Arabic art and culture in light of the power of fairy tales to create cross-border imagination. 
